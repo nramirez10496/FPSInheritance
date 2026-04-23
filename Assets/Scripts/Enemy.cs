7abd204744cf1b586991b69a87ae8d9f88e8d3e0
nramirez10496 <nramirez10496@gmail.com>
2026-04-23 17:07:59 -0500

    Enemy state functions and variables created

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,11 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//state machine
+enum EnemyStates
+{
+    wander,
+    pursue,
+    wait,
+    attack
+}
+
 public class Enemy : MonoBehaviour
 {
     public Rigidbody Rigidbody { get; private set; }
     Vector3 origin;
-    
+
+    //current state
+    [SerializeField]EnemyStates enemyCurrentState;
+
+    //AI variables
+    [SerializeField] float wanderRange;//how far from the starting point should the enemy be allowed to wander
+    [SerializeField] Vector3 startingLocation; //enemy’s starting location, set on start/spawn
+    [SerializeField] float playerSightRange; //how close should the player have to be for the enemy to see 
+    [SerializeField] float playerAttackRange;//how close should the player be before the enemy will lunge at them
+    [SerializeField] float currentStateElapsed; //how much time has passed in the current state (reset on changing state)
+    [SerializeField] float recoveryTime;//how long should the recovery state last before the enemy switches back
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +39,25 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //AI 
+        switch(enemyCurrentState)
+        {
+            case EnemyStates.wander:
+                UpdateWander();
+                break;
 
+            case EnemyStates.pursue:
+                UpdatePursue();
+                break;
+
+            case EnemyStates.wait:
+                UpdateWait();
+                break;
+
+            case EnemyStates.attack:
+                UpdateAttack();
+                break;
+        }
     }
 
     public void ApplyKnockback(Vector3 knockback)
@@ -28,5 +68,26 @@ public class Enemy : MonoBehaviour
     public void Respawn()
     {
         transform.position = origin;
+    }
+
+    //update functions for each state
+    void UpdateWander()
+    {
+
+    }
+
+    void UpdatePursue()
+    {
+
+    }
+
+    void UpdateWait()
+    {
+
+    }
+
+    void UpdateAttack()
+    {
+
     }
 }
