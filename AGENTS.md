2a6193a8b5a45a95925a5d46886fc5237934466f
nramirez10496 <nramirez10496@gmail.com>
2026-04-23 19:36:06 -0500

    Update pursue and recovery done + transition states + reinstated Player Input

--- AGENTS.md
+++ AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: FPSInheritance
 - Unity version: Unity 6000.0.66f2
 - Active game object:
-  - Name: Jeff
+  - Name: Player
   - Tag: Untagged
-  - Layer: Enemy
+  - Layer: Player
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 
 //state machine
@@ -8,39 +9,43 @@ enum EnemyStates
 {
     wander,
     pursue,
-    wait,
-    attack
+    attack,
+    recovery    
 }
 
 public class Enemy : MonoBehaviour
 {
     public Rigidbody Rigidbody { get; private set; }
-    Vector3 origin;
+    //Vector3 origin;
 
     //current state
     [SerializeField]EnemyStates enemyCurrentState;
 
     //AI variables
-    [SerializeField] float wanderRange;//how far from the starting point should the enemy be allowed to wander
-    [SerializeField] Vector3 startingLocation; //enemy’s starting location, set on start/spawn
-    [SerializeField] float playerSightRange; //how close should the player have to be for the enemy to see 
-    [SerializeField] float playerAttackRange;//how close should the player be before the enemy will lunge at them
-    [SerializeField] float currentStateElapsed; //how much time has passed in the current state (reset on changing state)
-    [SerializeField] float recoveryTime;//how long should the recovery state last before the enemy switches back
-
+    [SerializeField] float wanderRange = 5f;//how far from the starting point should the enemy be allowed to wander
+    Vector3 startingLocation; //enemy’s starting location, set on start/spawn
+    [SerializeField] float playerSightRange =10f; //how close should the player have to be for the enemy to see 
+    [SerializeField] float playerAttackRange = 2f;//how close should the player be before the enemy will lunge at them
+    [SerializeField] float currentStateElapsed = 0f; //how much time has passed in the current state (reset on changing state)
+    [SerializeField] float recoveryTime = 3f;//how long should the recovery state last before the enemy switches back
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Transform target;
 
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody = GetComponent<Rigidbody>();
-        origin = transform.position;
+        startingLocation = transform.position;
+        //origin = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //count time
+        currentStateElapsed += Time.deltaTime;
         //AI 
-        switch(enemyCurrentState)
+        switch (enemyCurrentState)
         {
             case EnemyStates.wander:
                 UpdateWander();
@@ -50,8 +55,8 @@ public class Enemy : MonoBehaviour
                 UpdatePursue();
                 break;
 
-            case EnemyStates.wait:
-                UpdateWait();
+            case EnemyStates.recovery:
+                UpdateRecovery();
                 break;
 
             case EnemyStates.attack:
@@ -67,7 +72,8 @@ public class Enemy : MonoBehaviour
 
     public void Respawn()
     {
-        transform.position = origin;
+        //transform.position = origin;
+        transform.position = startingLocation;
     }
 
     //update functions for each state
@@ -78,16 +84,67 @@ public class Enemy : MonoBehaviour
 
     void UpdatePursue()
     {
+        agent.isStopped = false;//moving again
 
+        //distance of enemy to player
+        float distanceToTarget = Vector3.Distance(transform.position, target.position);
+        
+        // Move toward player
+        agent.SetDestination(target.position);
+
+        //enemy is close enough to attack
+        if (distanceToTarget <= playerAttackRange)
+        {
+            PursueToAttack();
+        }
+
+        //enemy is not close to player
+        else if(distanceToTarget > playerSightRange)
+        {
+            PursueToWander();
+        }
     }
 
-    void UpdateWait()
+    void UpdateRecovery()
     {
+        //stop movement for recovery
+        agent.isStopped = true; 
 
+        //recovery time is over, cont pursue
+        if (currentStateElapsed >= recoveryTime)
+        {
+            RecoveryToPursue();
+        }
     }
 
     void UpdateAttack()
     {
 
+    }
+
+    //transition states
+    void WanderToPursue()
+    {
+        enemyCurrentState = EnemyStates.pursue;
+    }
+
+    void PursueToAttack()
+    { 
+        enemyCurrentState = EnemyStates.attack;
+    }
+
+    void AttackToRecovery()
+    {
+        enemyCurrentState = EnemyStates.recovery;
+    }
+
+    void RecoveryToPursue()
+    {
+        enemyCurrentState = EnemyStates.pursue;
+    }
+
+    void PursueToWander()
+    {
+        enemyCurrentState = EnemyStates.wander;
     }
 }
