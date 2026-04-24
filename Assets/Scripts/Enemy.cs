b1f6650ff5d929fd6da76b8937a7c2786333843c
nramirez10496 <nramirez10496@gmail.com>
2026-04-23 21:53:12 -0500

    UpdateWander done

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -24,26 +24,34 @@ public class Enemy : MonoBehaviour
     //AI variables
     [SerializeField] float wanderRange = 5f;//how far from the starting point should the enemy be allowed to wander
     Vector3 startingLocation; //enemy’s starting location, set on start/spawn
-    [SerializeField] float playerSightRange =10f; //how close should the player have to be for the enemy to see 
-    [SerializeField] float playerAttackRange = 2f;//how close should the player be before the enemy will lunge at them
+    [SerializeField] float playerSightRange =4f; //how close should the player have to be for the enemy to see 
+    [SerializeField] float playerAttackRange = 1f;//how close should the player be before the enemy will lunge at them
     [SerializeField] float currentStateElapsed = 0f; //how much time has passed in the current state (reset on changing state)
-    [SerializeField] float recoveryTime = 3f;//how long should the recovery state last before the enemy switches back
+    [SerializeField] float recoveryTime = 1f;//how long should the recovery state last before the enemy switches back
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Transform target;
+    float distanceToTarget;//distance of enemy to player 
+    Vector3 wanderPoint;//point enemy wanders to
 
     // Start is called before the first frame update
     void Start()
     {
         Rigidbody = GetComponent<Rigidbody>();
         startingLocation = transform.position;
+        wanderPoint = startingLocation;//initialize wander point
+        NewWanderPoint();
         //origin = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //distance of enemy to player
+        distanceToTarget = Vector3.Distance(transform.position, target.position);
+
         //count time
         currentStateElapsed += Time.deltaTime;
+
         //AI 
         switch (enemyCurrentState)
         {
@@ -76,29 +84,58 @@ public class Enemy : MonoBehaviour
         transform.position = startingLocation;
     }
 
+    void NewWanderPoint()
+    {
+        for (int i = 0; i < 30; i++) // Try multiple times to find a valid spot
+        {
+            //create random point to go to 
+            Vector3 randomPoint = Random.insideUnitSphere * wanderRange + startingLocation;
+
+            NavMeshHit hit;
+
+            //check randompoint is on surface
+            if (NavMesh.SamplePosition(randomPoint, out hit, wanderRange, NavMesh.AllAreas))
+            {
+                //update new point and move towards it
+                wanderPoint = hit.position;
+                agent.SetDestination(wanderPoint);
+                return;
+            }
+        }
+    }
+
     //update functions for each state
     void UpdateWander()
     {
+        agent.isStopped = false;//moving again
+        //enemy is close to player, pursue
+        if (distanceToTarget <= playerSightRange)
+        {
+            WanderToPursue();
+            return;
+        }
 
+        //enemy reacher random point, set new one
+        if(!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            NewWanderPoint();
+        }
     }
 
     void UpdatePursue()
     {
         agent.isStopped = false;//moving again
 
-        //distance of enemy to player
-        float distanceToTarget = Vector3.Distance(transform.position, target.position);
-        
         // Move toward player
         agent.SetDestination(target.position);
 
-        //enemy is close enough to attack
+        //enemy is close enough to attack, attack
         if (distanceToTarget <= playerAttackRange)
         {
             PursueToAttack();
         }
 
-        //enemy is not close to player
+        //enemy is not close to player, return to wander
         else if(distanceToTarget > playerSightRange)
         {
             PursueToWander();
@@ -119,32 +156,39 @@ public class Enemy : MonoBehaviour
 
     void UpdateAttack()
     {
+        Debug.Log("ATTACK!");
 
+        AttackToRecovery();
     }
 
     //transition states
     void WanderToPursue()
     {
         enemyCurrentState = EnemyStates.pursue;
+        currentStateElapsed = 0f;
     }
 
     void PursueToAttack()
     { 
         enemyCurrentState = EnemyStates.attack;
+        currentStateElapsed = 0f;
     }
 
     void AttackToRecovery()
     {
         enemyCurrentState = EnemyStates.recovery;
+        currentStateElapsed = 0f;
     }
 
     void RecoveryToPursue()
     {
         enemyCurrentState = EnemyStates.pursue;
+        currentStateElapsed = 0f;
     }
 
     void PursueToWander()
     {
         enemyCurrentState = EnemyStates.wander;
+        currentStateElapsed = 0f;
     }
 }
